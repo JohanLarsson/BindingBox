534b2bc585616d7927a2d621da5a7e69d595d038
Johan Larsson <johan_gavle1@hotmail.com>
2014-09-21 17:50:03 +0200

    Initial

---
+++ BindingBox/ViewModel.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BindingBox
+{
+    using System.ComponentModel;
+    using System.Reflection.Emit;
+    using System.Runtime.CompilerServices;
+    using Annotations;
+
+    public class ViewModel : INotifyPropertyChanged
+    {
+        private string _firstName;
+        private string _lastName;
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string FirstName
+        {
+            get { return _firstName; }
+            set
+            {
+                if (value == _firstName)
+                {
+                    return;
+                }
+                _firstName = value;
+                OnPropertyChanged();
+                OnPropertyChanged("FullName");
+            }
+        }
+
+        public string LastName
+        {
+            get { return _lastName; }
+            set
+            {
+                if (value == _lastName)
+                {
+                    return;
+                }
+                _lastName = value;
+                OnPropertyChanged();
+                OnPropertyChanged("FullName");
+            }
+        }
+
+        public string FullName
+        {
+            get { return string.Format("{0} {1}", FirstName, LastName); }
+        }
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
